f1c547277b7415862af622d108b6ed28616f3fa9
Nidaoke <jnidaokefantagefacebook27@gmail.com>
2016-06-10 16:22:44 -0400

    First Setup + Basic Code
    Imported Objects. (Created materials + textures folders to store models'
    components; Reorganized the models + materials, as some parts were named
    incorrectly; Created some prefabs from the models + Added them to the
    scene with a basic layout)
    Added waypoints to the board for respected players, both red and blue
    (Read further for explanation)
    Created a basic script that allows player to move using W and S (Or
    anything else that Unity considers the Vertical Axis to be). The player
    will move along a set path of waypoints, but can stop anywhere
    in-between them and continue in the opposite direction. As the player
    reaches a waypoint, the script sets his next and last waypoints to the
    following waypoint to reach, and the one he just passed.
    The same script contains a Turning section, which is activated by the
    Horizontal axis. When a player is turned around (facing the opposite
    direction), their forward and backwards code is swapped, so they will
    basically move in the direction the are facing. Players have an active
    bool, which allows them to move and turn. This will set who the player
    is controlling. They also have a blue/red enum, to set which team they
    are on. There are 2 speed settings, one for moving (10 by default), and
    one for turning (125 by default).
    In the next push, I'll try to get the camera to move towards a player as
    they become active, and then allow the player to change who they are
    controlling.

---
+++ Assets/_Scripts/PlayerMovementScript.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementScript : MonoBehaviour {
+
+    public enum DirectionToMove { forward, back, still }; public DirectionToMove directionToMove; //Should player move forward or back
+    public enum ColorOfPlayer { red, blue }; public ColorOfPlayer colorOfPlayer;
+    public int lastWaypoint, nextWaypoint;
+    public Transform[] waypoints; //Set Waypoints for player to move along
+    public float speed, turningSpeed;
+    public bool activePlayer, turnedAround;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void Update () {
+        Move();
+        Turn();
+	}
+
+    void Turn()
+    {
+        if (activePlayer)
+        {
+            if (Input.GetAxis("Horizontal") > .2f) //Rotate Around
+            {
+                transform.Rotate(Vector3.up * Time.deltaTime * turningSpeed);
+            }
+            else if (Input.GetAxis("Horizontal") < -.2f)
+            {
+                transform.Rotate(Vector3.down * Time.deltaTime * turningSpeed);
+            }
+
+            if(colorOfPlayer == ColorOfPlayer.blue) //Set Turned Around if facing other direction
+            {
+                if (transform.localRotation.eulerAngles.y > 270 || transform.localRotation.eulerAngles.y < 90)
+                    turnedAround = true;
+                else
+                    turnedAround = false;
+            }else
+            {
+                if (transform.localRotation.eulerAngles.y < 270 && transform.localRotation.eulerAngles.y > 90)
+                    turnedAround = true;
+                else
+                    turnedAround = false;
+            }
+        }
+    }
+
+    void Move()
+    {
+        if (activePlayer) //Only move when this player is active (Overwrite w/ AI later)
+            GetInputForMovement();
+
+        switch (directionToMove) //Call MoveToPoint depending on our direction to move
+        {
+            case DirectionToMove.forward:
+                MoveToPoint(waypoints[nextWaypoint]);
+                break;
+            case DirectionToMove.back:
+                MoveToPoint(waypoints[lastWaypoint]);
+                break;
+            case DirectionToMove.still:
+                //Don't Do Anything
+                break;
+        }
+
+        if(transform.position == waypoints[nextWaypoint].position) //Switch waypoints, allowing us to move on curves
+        {
+            if (nextWaypoint < waypoints.Length-1)
+            {
+                lastWaypoint++;
+                nextWaypoint++;
+            }
+        }else if(transform.position == waypoints[lastWaypoint].position)
+        {
+            if (lastWaypoint > 0)
+            {
+                lastWaypoint--;
+                nextWaypoint--;
+            }
+        }
+    }
+
+    void MoveToPoint(Transform point) //Move toward Waypoint
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, point.position, step);
+    }
+
+    void GetInputForMovement() //Get input for moving
+    {
+        if (!turnedAround)
+        {
+            if (Input.GetAxis("Vertical") > .2f) //Set the direction to move based on input
+                directionToMove = DirectionToMove.forward;
+            else if (Input.GetAxis("Vertical") < -.2f)
+                directionToMove = DirectionToMove.back;
+            else
+                directionToMove = DirectionToMove.still;
+        }else
+        {
+            if (Input.GetAxis("Vertical") > .2f) //Move in the opposite direction if turned around
+                directionToMove = DirectionToMove.back;
+            else if (Input.GetAxis("Vertical") < -.2f)
+                directionToMove = DirectionToMove.forward;
+            else
+                directionToMove = DirectionToMove.still;
+        }
+    }
+}
